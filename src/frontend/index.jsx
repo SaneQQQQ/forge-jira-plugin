8e3ef897b226e6ce0e212335d79559525790d998
okarabush <okarabush@atlassian.com>
2025-08-22 15:53:40 +0300

    FJP-1: Created an Auth page to save GitHub PAT TOKEN

--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -1,18 +1,143 @@
-import React, { useEffect, useState } from 'react';
-import ForgeReconciler, { Text } from '@forge/react';
+import React, { useState, useEffect } from 'react';
 import { invoke } from '@forge/bridge';
+import ForgeReconciler, {
+    Box,
+    ErrorMessage,
+    Form,
+    FormFooter,
+    FormHeader,
+    FormSection,
+    HelperMessage,
+    Label,
+    Link,
+    LoadingButton,
+    RequiredAsterisk,
+    SectionMessage,
+    Stack,
+    Text,
+    Textfield,
+    useForm,
+    ValidMessage
+} from '@forge/react';
+
 const App = () => {
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    invoke('getText', { example: 'my-invoke-variable' }).then(setData);
-  }, []);
-  return (
-    <>
-      <Text>Hello world!</Text>
-      <Text>{data ? data : 'Loading...'}</Text>
-    </>
-  );
+    const {handleSubmit, register, getFieldId, formState} = useForm();
+    const {errors, touchedFields} = formState;
+    const [isLoginError, setIsLoginError] = useState(false);
+    const [isSaved, setIsSaved] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [token, setToken] = useState(null);
+
+    const isValidToken = async (token) => {
+        return await invoke('isValidToken', {token: token});
+    };
+
+    const saveToken = async (token) => {
+        return await invoke('saveToken', {token: token});
+    }
+
+    const getToken = async () => {
+        return await invoke('getToken');
+    }
+
+    const deleteToken = async () => {
+        return await invoke('deleteToken');
+    }
+
+    const submitToken = async (data) => {
+        setIsLoading(true);
+        setIsLoginError(false);
+        setIsSaved(false);
+
+        if (!await isValidToken(data.token)) {
+            setIsLoginError(true);
+            setIsLoading(false);
+            return;
+        }
+
+        if (await saveToken(data.token)) {
+            setIsSaved(true);
+        }
+
+        setIsLoading(false);
+    };
+
+    useEffect(() => {
+        const populateToken = async () => {
+            const token = await getToken();
+            setToken(token);
+        }
+
+        populateToken();
+    }, [getToken, setToken]);
+
+    return (
+        <>
+            <Form onSubmit={handleSubmit(submitToken)}>
+                <FormHeader title="GitHub Personal Access Token">
+                    <Text>
+                        Enter your GitHub Personal Access Token (PAT) to continue. Learn how to create one:{' '}
+                        <Link href="https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens" target="_blank">
+                            Managing your personal access tokens.
+                        </Link>
+                    </Text>
+                </FormHeader>
+                <FormSection>
+                    {isSaved && !isLoginError && (
+                        <SectionMessage appearance="success">
+                            <Text>
+                                Your token has been successfully saved.
+                            </Text>
+                        </SectionMessage>
+                    )}
+                    {isLoginError && (
+                        <SectionMessage appearance="error">
+                            <Text>
+                                Your token is invalid or expired. Please generate a new one and try again.
+                            </Text>
+                        </SectionMessage>
+                    )}
+                    <Stack space="space.100">
+                        {!isSaved && (
+                            <Box>
+                                <Label labelFor={getFieldId("token")}>
+                                    Personal Access Token
+                                    <RequiredAsterisk />
+                                </Label>
+                                <Textfield placeholder="Place your token here" {...register("token", {
+                                    required: true,
+                                    pattern: /^(gh[ps]_[a-zA-Z0-9]{36}|github_pat_[a-zA-Z0-9]{22}_[a-zA-Z0-9]{59})$/ })} />
+                                {errors["token"] && (
+                                    <ErrorMessage>
+                                        Invalid token format. Please check and try again.
+                                    </ErrorMessage>
+                                )}
+                                {!touchedFields["token"] && !errors["token"] && (
+                                    <HelperMessage>
+                                        Use a fine-grained or classic PAT generated in your GitHub account settings.
+                                    </HelperMessage>
+                                )}
+                                {touchedFields["token"] && !errors["token"] && (
+                                    <ValidMessage>
+                                        Looks good! Valid token format.
+                                    </ValidMessage>
+                                )}
+                            </Box>
+                        )}
+                    </Stack>
+                </FormSection>
+                {!isSaved && (
+                    <FormFooter>
+                        <LoadingButton isLoading={isLoading} appearance="primary" type="submit">
+                            Save Token
+                        </LoadingButton>
+                    </FormFooter>
+                )}
+            </Form>
+        </>
+    );
 };
+
 ForgeReconciler.render(
   <React.StrictMode>
     <App />
--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,10 +1,45 @@
 import Resolver from '@forge/resolver';
+import { Octokit } from "octokit";
+import { kvs } from "@forge/kvs";
 
 const resolver = new Resolver();
 
-resolver.define('getText', (req) => {
-  console.log(req);
-  return 'Hello, world!';
+resolver.define('isValidToken', async (req) => {
+    const octokit = new Octokit({
+        auth: req.payload.token,
+    });
+
+    try {
+        const response = await octokit.request('GET /user/repos', {
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28',
+          },
+        });
+
+        return response.status === 200;
+    } catch (err) {
+        throw new Error('Token validation check failed');
+    }
 });
+
+resolver.define('saveToken', async (req) => {
+    const token = req.payload.token;
+    if (!token) {
+        throw new Error('Token is required');
+    }
+
+    await kvs.setSecret('token', token);
+    return true;
+});
+
+resolver.define('getToken', async () => {
+    const token = await kvs.getSecret('token');
+    return token ? token : '';
+});
+
+resolver.define('deleteToken', async () => {
+    await kvs.deleteSecret('token');
+    return true;
+})
 
 export const handler = resolver.getDefinitions();
